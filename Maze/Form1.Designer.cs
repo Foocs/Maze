5c6927c7590fccb0e6729f30f678662a7655ccb3
Foocs <40733875+Foocs@users.noreply.github.com>
2019-11-29 23:51:32 +0200

    Upload files

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pb_Player = new System.Windows.Forms.PictureBox();
+            this.movementUpdate = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_Player
+            // 
+            this.pb_Player.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pb_Player.BackColor = System.Drawing.Color.Maroon;
+            this.pb_Player.Location = new System.Drawing.Point(121, 368);
+            this.pb_Player.Name = "pb_Player";
+            this.pb_Player.Size = new System.Drawing.Size(83, 77);
+            this.pb_Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_Player.TabIndex = 0;
+            this.pb_Player.TabStop = false;
+            // 
+            // movementUpdate
+            // 
+            this.movementUpdate.Enabled = true;
+            this.movementUpdate.Interval = 1;
+            this.movementUpdate.Tick += new System.EventHandler(this.movementUpdate_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(249, 306);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 148);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel2.Location = new System.Drawing.Point(206, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(69, 155);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(321, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(549, 588);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb_Player);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_Player;
+        private System.Windows.Forms.Timer movementUpdate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            playerBoundsUpdate();
+        }
+
+        #region Variables
+
+        int movementSpeed = 10;
+
+        bool up = false;
+        bool down = false; 
+        bool left = false;
+        bool right = false;
+
+        static Rectangle pb_Bounds_Up;
+        static Rectangle pb_Bounds_Down;
+        static Rectangle pb_Bounds_Left;
+        static Rectangle pb_Bounds_Right;
+
+        static int collisionUp;
+        static int collisionDown;
+        static int collisionLeft;
+        static int collisionRight;
+
+        #endregion
+
+        #region Functions
+
+        private void playerBoundsUpdate()
+        {
+            pb_Bounds_Up = new Rectangle(pb_Player.Left, pb_Player.Top - movementSpeed, pb_Player.Width, 1);
+            pb_Bounds_Down = new Rectangle(pb_Player.Left, pb_Player.Bottom + movementSpeed, pb_Player.Width, 1);
+            pb_Bounds_Left = new Rectangle(pb_Player.Left - movementSpeed, pb_Player.Top, 1, pb_Player.Height);
+            pb_Bounds_Right = new Rectangle(pb_Player.Right + movementSpeed, pb_Player.Top, 1, pb_Player.Height);
+
+            collisionUp = -1;
+            collisionDown = -1;
+            collisionLeft = -1;
+            collisionRight = -1;
+        }
+        #region Movement
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    up = true;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    down = true;
+                    break;
+                case Keys.A:
+                case Keys.Left:
+                    left = true;
+                    break;
+                case Keys.D:
+                case Keys.Right:
+                    right = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                case Keys.Up:
+                    up = false;
+                    break;
+                case Keys.S:
+                case Keys.Down:
+                    down = false;
+                    break;
+                case Keys.A:
+                case Keys.Left:
+                    left = false;
+                    break;
+                case Keys.D:
+                case Keys.Right:
+                    right = false;
+                    break;
+            }
+        }
+
+        private void movementUpdate_Tick(object sender, EventArgs e)
+        {
+            foreach (var pan in this.Controls.OfType<Panel>())
+            {
+                if (pb_Bounds_Up.IntersectsWith(pan.Bounds))
+                    collisionUp = pb_Player.Top - pan.Bottom;
+
+                if (pb_Bounds_Down.IntersectsWith(pan.Bounds))
+                    collisionDown = pan.Top - pb_Player.Bottom;
+
+                if (pb_Bounds_Left.IntersectsWith(pan.Bounds))
+                    collisionLeft = pb_Player.Left - pan.Right;
+
+                if (pb_Bounds_Right.IntersectsWith(pan.Bounds))
+                    collisionRight = pan.Left - pb_Player.Right;
+            }
+
+            if (up)
+                if (pb_Player.Top >= movementSpeed)
+                    if (collisionUp == -1)
+                        pb_Player.Top -= movementSpeed;
+                    else
+                        pb_Player.Top -= collisionUp;
+                else
+                    pb_Player.Top = 0;
+
+            if (down)
+                if (pb_Player.Bottom <= this.ClientRectangle.Height - movementSpeed)
+                    if (collisionDown == -1)
+                        pb_Player.Top += movementSpeed;
+                    else
+                        pb_Player.Top += collisionDown;
+                else
+                    pb_Player.Top = this.ClientRectangle.Bottom - pb_Player.Height;
+
+
+            if (left)
+                if (pb_Player.Left >= movementSpeed)
+                    if (collisionLeft == -1)
+                        pb_Player.Left -= movementSpeed;
+                    else
+                        pb_Player.Left -= collisionLeft;
+                else
+                    pb_Player.Left = 0;
+
+
+            if (right)
+                if (pb_Player.Right <= this.ClientRectangle.Width - movementSpeed)
+                    if (collisionRight == -1)
+                        pb_Player.Left += movementSpeed;
+                    else
+                        pb_Player.Left += collisionRight;
+                else
+                    pb_Player.Left = this.ClientRectangle.Right - pb_Player.Width;
+
+            playerBoundsUpdate();
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
